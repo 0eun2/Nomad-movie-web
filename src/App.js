7fd1ebabbe3045dfb260942774e98bc8efa1bb0b
0eun2 <youngeun.choi26@gmail.com>
2025-07-23 22:27:32 +0900

    add: create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  //const API_KEY = process.env.REACT_APP_KOBIS_API_KEY;
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    ).then((response) => response.json())
+      .then((json) => setMovies(json.data.movies));
+  }, []);
+  console.log(movies);
+  return <div> {loading ? <h1>Loading...</h1> : null}</div>;
+}
+
+export default App;
